b52007a68534620ac1b79ad71fe6a66f85ffca5b
kodeape <sarargh@stud.ntnu.no>
2023-03-15 12:13:14 +0100

    Changed submit-all results directory

--- submit-all.sh
+++ submit-all.sh
@@ -6,12 +6,21 @@ DIRECTORY=/cluster/home/sarargh/sniper_traces/spec_cpu2017_reference_w0_d1B
 # Get the list of subfolders
 SUBFOLDERS=$(find $DIRECTORY/* -maxdepth 0 -type d)
 
+# Clear/make the results directory
+if [ -d "./results" ]; then
+  rm -r "./results"
+fi
+mkdir "./results"
+
 # Loop through each subfolder
 for folder in $SUBFOLDERS; do
   # Get the job name
   job_name=$(basename $folder)
 
+  # Make the job result directory
+  mkdir "./results/$job_name"
+
   # Run the slurm script with the job name, max time, and command
-  ./slurm-submit.sh $job_name 02:00:00 "./run-sniper -c rakesh.cfg -c robOoO_000_NoQ_256ROB.cfg -d $DIRECTORY/$job_name --traces=$DIRECTORY/$job_name/${job_name}_trace.sift"
+  ./slurm-submit.sh $job_name 02:00:00 "./run-sniper -c rakesh.cfg -c robOoO_000_NoQ_256ROB.cfg -d ./results/$job_name --traces=$DIRECTORY/$job_name/${job_name}_trace.sift"
 
 done
